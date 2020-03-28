42970b3d4b777242b92f1ca773a6224aecbfd2fe
DavidMulder0 <david@apriol.com>
2020-03-28 16:57:56 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,110 @@
-declare module 'vue-material/dist/components';
+
+declare module 'vue-material/dist/components' {
+    import { PluginFunction } from 'vue';
+
+    
+                export const MdApp: PluginFunction<any>;
+            
+
+                export const MdAutocomplete: PluginFunction<any>;
+            
+
+                export const MdAvatar: PluginFunction<any>;
+            
+
+                export const MdBadge: PluginFunction<any>;
+            
+
+                export const MdBottomBar: PluginFunction<any>;
+            
+
+                export const MdButton: PluginFunction<any>;
+            
+
+                export const MdCard: PluginFunction<any>;
+            
+
+                export const MdCheckbox: PluginFunction<any>;
+            
+
+                export const MdChips: PluginFunction<any>;
+            
+
+                export const MdContent: PluginFunction<any>;
+            
+
+                export const MdDatepicker: PluginFunction<any>;
+            
+
+                export const MdDialog: PluginFunction<any>;
+            
+
+                export const MdDivider: PluginFunction<any>;
+            
+
+                export const MdDrawer: PluginFunction<any>;
+            
+
+                export const MdElevation: PluginFunction<any>;
+            
+
+                export const MdEmptyState: PluginFunction<any>;
+            
+
+                export const MdField: PluginFunction<any>;
+            
+
+                export const MdHighlightText: PluginFunction<any>;
+            
+
+                export const MdIcon: PluginFunction<any>;
+            
+
+                export const MdImage: PluginFunction<any>;
+            
+
+                export const MdLayout: PluginFunction<any>;
+            
+
+                export const MdList: PluginFunction<any>;
+            
+
+                export const MdMenu: PluginFunction<any>;
+            
+
+                export const MdProgress: PluginFunction<any>;
+            
+
+                export const MdRadio: PluginFunction<any>;
+            
+
+                export const MdRipple: PluginFunction<any>;
+            
+
+                export const MdSnackbar: PluginFunction<any>;
+            
+
+                export const MdSpeedDial: PluginFunction<any>;
+            
+
+                export const MdSteppers: PluginFunction<any>;
+            
+
+                export const MdSubheader: PluginFunction<any>;
+            
+
+                export const MdSwitch: PluginFunction<any>;
+            
+
+                export const MdTable: PluginFunction<any>;
+            
+
+                export const MdTabs: PluginFunction<any>;
+            
+
+                export const MdToolbar: PluginFunction<any>;
+            
+
+                export const MdTooltip: PluginFunction<any>;
+            
+}    
